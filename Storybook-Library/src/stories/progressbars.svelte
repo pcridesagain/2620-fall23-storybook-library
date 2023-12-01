82091509a679295d2802ba1d05c41986e61d913a
addilewis21 <addilewis21@gmail.com>
2023-11-30 18:36:17 -0700

    added story files

---
+++ Storybook-Library/src/stories/progressbars.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let progress = 50; // Initial progress value (percentage)
+</script>
+
+<style>
+  .progress-bar {
+    width: 100%;
+    background-color: #e0e0e0;
+    height: 20px;
+    border-radius: 4px;
+    overflow: hidden;
+  }
+
+  .progress {
+    width: 10%;
+    height: 100%;
+    background-color: #4caf50;
+    transition: width 0.3s ease-in-out;
+  }
+</style>
+
+<div class="progress-bar">
+  <div class="progress"></div>
+</div>
+
