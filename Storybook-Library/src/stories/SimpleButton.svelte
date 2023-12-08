bacbadcf42921a3b98fb9370c1e2081d0758a8d4
Michael Dahle <91270944+Mike-Dahle@users.noreply.github.com>
2023-12-07 18:35:31 -0700

    Comitting my button

---
+++ Storybook-Library/src/stories/SimpleButton.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let text = "Cancel";
+  export let color = "cancel";
+  export let size = "md";
+  let colors = {
+      cancel: 'bg-[#e7e7ff] text-[#6b4eff] hover:bg-[#d6d6ff] hover:text-[#6b4eff]',
+      active: 'bg-[#6b4eff] text-[#ffffff] hover:bg-[#5a3dc8] hover:text-[#ffffff]'
+  };
+  let sizes = {
+      sm: 'px-[16px] h-[32px]',
+      md: 'px-[52px] h-[48px]',
+      lg: 'w-[155.5px] h-[48px]'
+  };
+</script>
+
+<button class={`rounded-full text-center border-none ${colors[color] || colors.cancel} ${sizes[size] || sizes.md}`}>
+  {text}
+</button>
